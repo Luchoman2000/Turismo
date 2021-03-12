514765d7133e7c856c7a110302fc4342aafac49d
Luis <52828089+Luchoman2000@users.noreply.github.com>
2021-03-12 11:04:44 -0500

    test production

--- core/configGeneral.php
+++ core/configGeneral.php
@@ -1,4 +1,4 @@
 <?php
-const SERVERURL = 'http://localhost/Turismo/';
+const SERVERURL = 'https://mejia360.herokuapp.com/';
 const COMPANY   = 'Mejia360';
 date_default_timezone_set('America/Guayaquil');
